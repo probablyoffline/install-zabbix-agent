65f455ac4905a3fff0240e04c51661cc9c595825
offline <8227731+probablyoffline@users.noreply.github.com>
2023-02-03 17:42:07 -0600

    Update install-zabbix-agent.sh

--- install-zabbix-agent.sh
+++ install-zabbix-agent.sh
@@ -2,8 +2,6 @@
 
 # Prompt for zabbix server
 read -p "Enter Zabbix server IP or FQDN: " zabbix_ipfqdn
-host_ip=$(hostname -I)
-host_string="$host_ip $zabbix_ipfqdn"
 
 # Update and install agent
 sudo apt update -y
@@ -12,12 +10,12 @@ sudo apt install zabbix-agent -y
 # Stop service and update config
 sudo systemctl stop zabbix-agent.service
 
-sudo sed -i 's/Server=127.0.0.1/Server=$zabbix_ipfqdn/g' /etc/zabbix/zabbix_agentd.conf
-sudo sed -i 's/# HostnameItem=system.hostname/HostnameItem=system.hostname/g' /etc/zabbix/zabbix_agentd.conf
-sudo sed -i 's/# HostMetadataItem=/HostMetadataItem=system.uname/g' /etc/zabbix/zabbix_agentd.conf
-sudo sed -i 's/ServerActive=127.0.0.1/ServerActive=$zabbix_ipfqdn/g' /etc/zabbix/zabbix_agentd.conf
-sudo sed -i 's/# StartAgents=3/StartAgents=1/g' /etc/zabbix/zabbix_agentd.conf
-sudo sed -i 's/# RefreshActiveChecks=120/RefreshActiveChecks=120/g' /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/Server=127.0.0.1/Server=$zabbix_ipfqdn/g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/# HostnameItem=system.hostname/HostnameItem=system.hostname/g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/# HostMetadataItem=/HostMetadataItem=system.uname/g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/ServerActive=127.0.0.1/ServerActive=$zabbix_ipfqdn/g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/# StartAgents=3/StartAgents=1/g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/# RefreshActiveChecks=120/RefreshActiveChecks=120/g" /etc/zabbix/zabbix_agentd.conf
 
 # Start agent
 sudo systemctl start zabbix-agent.service
