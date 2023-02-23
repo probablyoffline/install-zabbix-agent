553d4e55b1304b1736c1391d831556b043e0c9bd
offline <8227731+probablyoffline@users.noreply.github.com>
2023-02-23 17:54:47 -0600

    Update install-zabbix-agent.sh

--- install-zabbix-agent.sh
+++ install-zabbix-agent.sh
@@ -14,7 +14,7 @@ sudo sed -i "s/Server=127.0.0.1/Server=$zabbix_ipfqdn/g" /etc/zabbix/zabbix_agen
 sudo sed -i "s/# HostnameItem=system.hostname/HostnameItem=system.hostname/g" /etc/zabbix/zabbix_agentd.conf
 sudo sed -i "s/# HostMetadataItem=/HostMetadataItem=system.uname/g" /etc/zabbix/zabbix_agentd.conf
 sudo sed -i "s/ServerActive=127.0.0.1/ServerActive=$zabbix_ipfqdn/g" /etc/zabbix/zabbix_agentd.conf
-sudo sed -i "s/# StartAgents=3/StartAgents=1/g" /etc/zabbix/zabbix_agentd.conf
+sudo sed -i "s/# StartAgents=3/StartAgents=0/g" /etc/zabbix/zabbix_agentd.conf
 sudo sed -i "s/# RefreshActiveChecks=120/RefreshActiveChecks=120/g" /etc/zabbix/zabbix_agentd.conf
 
 # Start agent
